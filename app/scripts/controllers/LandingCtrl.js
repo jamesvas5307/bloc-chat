ea657d59adba026613af9f21b4b51a2fd5ffa8ec
James Vasquez <jamesvas5307@live.com>
2017-06-05 17:47:38 -0400

    Checkpoint 5 Set Username

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,10 +1,24 @@
 (function(){
- function LandingCtrl($scope, Chatrooms){
+ function LandingCtrl($scope ,Chatrooms){
    this.rooms = Chatrooms.all;
-
  }
+ function BlocChatCookies($rootScope,$uibModal) {
+  var currentUser = window.localStorage.getItem('blocChatCurrentUser');
+  if (!currentUser || currentUser === '') {
+    $rootScope.modalInstance = $uibModal.open({
+       ariaLabelledBy: 'modal-title',
+       ariaDescribedBy: 'modal-body',
+       backdrop: true,
+       templateUrl: '../../templates/usernameModal.html',
+       controller: 'UsernameModalCtrl',
+       controllerAs: '$ctrl',
+       size: 'lg'
+     });
+  }
+}
  angular
   .module('blocChat')
-  .controller('LandingCtrl',['$scope', 'Chatrooms',LandingCtrl])
+  .controller('LandingCtrl',['$scope', '$rootScope', '$uibModal', 'Chatrooms',LandingCtrl])
+  .run(BlocChatCookies);
 
 })();
---
+++ app/scripts/controllers/UsernameModalCtrl.js
@@ -0,0 +1,20 @@
+(function(){
+ function UsernameModalCtrl($uibModal, $rootScope, Chatrooms){
+
+  this.ok = function(){
+    var userName = document.querySelector("#username-form").value;
+    console.log(userName);
+    if(userName){
+    window.localStorage.setItem('blocChatCurrentUser', userName)
+    $rootScope.modalInstance.close();
+  }
+  };
+  this.cancel = function($uiModal){
+    $rootScope.modalInstance.close();
+  };
+ }
+ angular
+  .module('blocChat')
+  .controller('UsernameModalCtrl',['$uibModal', '$rootScope', 'Chatrooms', UsernameModalCtrl])
+
+})();
